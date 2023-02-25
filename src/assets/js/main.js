4931fbabbb32d3db69ef34ad02cd2d7c2402121c
hansapradhan <pradhan.hansa@gmail.com>
2023-02-25 18:33:35 +0530

    Committed Code to modify skill section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,64 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function () {
 
-$(document).ready(function() {
   AOS.init( {
     // uncomment below for on-scroll animations to played only once
     // once: true
 
   }); // initialize animate on scroll library
+
+	function hex_initial_animation() {
+		$(".hex-wrap,.hover-notify").velocity("transition.expandIn", { stagger: 150 });
+		$(".hex-wrap").velocity("callout.pulse");
+		$(".hoverblock").velocity("fadeOut", { delay: 3000, duration: 0 });
+		}
+	hex_initial_animation();
+
+var hoverdetect = setInterval(function(){ hovernotify() }, 3000);
+function hovernotify() {
+    $(".hover-notify").velocity("callout.tada");
+}
+function myStopFunction() {
+$(".hover-notify").velocity('stop', true).velocity("fadeOut");
+    clearInterval(hoverdetect);
+}
+
+		$(".hex-init").mouseenter(function () {
+			
+			myStopFunction();
+
+			var title_color =  $(this).parent().attr("data-color");
+			var title_name = $(this).parent().attr("data-title");
+			var desc_name = $(this).parent().attr("data-content");
+
+				function hex_description() {
+					$('.code-description').velocity('stop', true).velocity("transition.slideRightBigIn");
+					$('.' + desc_name).siblings().removeClass('desc-active');
+						setTimeout(function() {
+							$('.' + desc_name).addClass('desc-active');
+							$('.code-descriptopn > div, .desc-active').children().velocity('stop', true).velocity("transition.slideRightBigIn", { stagger: 300 });
+							$('.code-title, .desc-active span').velocity({color: title_color}, { queue: false });
+							$('.code-title').text(title_name)
+						}, 0);
+			    }
+			    hex_description();
+
+				$(this).parent().addClass('hexactive'); 
+				$('.hexactive').velocity({scaleX:"1.1",scaleY:"1.1"}, { duration: 200 });
+
+			}).mouseleave(function () {
+				 $('.hexactive').velocity('stop', true).velocity('reverse').removeClass('hexactive');
+			});
 });
+
+// $(document).ready(function() {
+//   AOS.init( {
+//     // uncomment below for on-scroll animations to played only once
+//     // once: true
+
+//   }); // initialize animate on scroll library
+// });
 
 
 function openqualification(evt, qualification) {
@@ -126,3 +177,4 @@ themeButton.addEventListener('click', () => {
     }
 })
 }
+
