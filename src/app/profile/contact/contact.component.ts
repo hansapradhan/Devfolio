9f494c56dfd8a004450f9d496896f2c66ba0d155
hansapradhan <pradhan.hansa@gmail.com>
2023-04-03 08:35:53 +0530

    commit for contact component fix

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders} from '@angular/common/http';
-import { ProfileService } from '../profile.service';
-import { SnotifyService } from 'ng-snotify';
-import { environment } from '../../../environments/environment';
 import { ContactFormModel } from './contact-form-model';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-//import Validation from './utils/validation';
 
 @Component({
   selector: 'app-contact',
@@ -14,7 +10,7 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
   styleUrls: ['./contact.component.scss']
 })
   export class ContactComponent implements OnInit {
- 
+    isFormSuccessful = "null";
     // model: any = {};
     //Model for the form
     contactModel = new ContactFormModel("","","","");
@@ -29,7 +25,9 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
     constructor(
       private http: HttpClient,
       private formBuilder: FormBuilder
-    ){}
+    ){
+
+    }
   
     ngOnInit() {
       this.contactform = this.formBuilder.group(
@@ -47,21 +45,46 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
     }
     // When the form is submitted
     onSubmit(): void {
+
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
           this.http.post('https://formcarry.com/s/Vuy8gmC-p',
             { name: this.contactModel.name, subject: this.contactModel.subject, replyto: this.contactModel.email, message: this.contactModel.message },
             { 'headers': headers }).subscribe(
               response => {
+                // var SuccesAlert = document.getElementById('success');
+                // var FailureAlert = document.getElementById('failure');
+
+                // if(document.getElementById('success')== null || document.getElementById('failure') == null)
+                //   {location.reload();}
+
+                // console.log(JSON.parse(JSON.stringify(response)).status);
+                // console.log(SuccesAlert?.style.display);
+                // if (JSON.parse(JSON.stringify(response)).status == 'success' && SuccesAlert) {
+                //   SuccesAlert.style.display = 'block';
+                // }
+                // else if(FailureAlert){
+                //   FailureAlert.style.display = 'block';
+                // }
+                
+                if( this.isFormSuccessful=="success" || this.isFormSuccessful=="failed")
+                  {location.reload();}
+                
+                if (JSON.parse(JSON.stringify(response)).status == 'success') {
+                  this.isFormSuccessful="success"
+                }
+                else{
+                  this.isFormSuccessful="failed"
+                }
                 console.log(response);
               }
             );
-
-      this.submitted = true;
+            
+            this.submitted = true;
   
-      if (this.contactform.invalid) {
-        return;
+            if (this.contactform.invalid) {
+              return;
+            }
 
-      }
       console.log(JSON.stringify(this.contactform.value, null, 2));
     }
 
